4e885cd309bb395e054e07a7c163e5e1385616d8
Jeffrey A. Teruel <jeffreyteruel@gmail.com>
2018-04-02 11:30:57 +0800

    landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,12 +14,21 @@
     background-repeat: no-repeat;  
     background-size: cover; height:500px;  
    } 
+   #header h1 { font-weight:bold; }
+   #header h3 { font-weight:bold; color:#fff;}
+   #header #welcomemessage { padding-top:150px; }
 </style>	
   <!--navigation links here. does it convert to click dropdown?-->  
  <!--end navigation bar; start header content--> 
  <div class="container">
    <div class="row">
-        <h1>ekeke</h1>
+      <div id="welcomemessage">
+        <h1>Craft Future Career Development Experience</h1>
+
+        <h3>Building Individuals for the Careers of the Future</h3>
+        <br/> 
+        <center><a href="/interest" class="btn btn-primary" style="padding:20px; padding-left:40px; padding-right:40px; ">Learn More</a></center>
+      </div>
    </div>
  </div> 
   
